297d4e0e20c5e6d3d4d4181728a192997190169d
mavroian <mavflorin@gmail.com>
2020-01-12 00:50:13 +0900

    importing  and routing ShopPage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css'
 import { Route, Switch } from "react-router-dom"
-
 import HomePage from "./pages/homepage/Homepage.component"
-const Hats = () => {
-  return (<div><h1>HatsPage</h1></div>)
-}
+import ShopPage from "./pages/shop/shop.component"
+
 function App() {
   return (
 
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
 
     </div>
