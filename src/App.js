4c26825a65d73747456bae9a1b4cafc9c5786a68
mavroian <mavflorin@gmail.com>
2020-01-11 23:04:28 +0900

    update menu-items to navigate to specific page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import { Route, Switch } from "react-router-dom"
 
-import HomePage from "./components/Homepage.component"
-
+import HomePage from "./pages/homepage/Homepage.component"
+const Hats = () => {
+  return (<div><h1>HatsPage</h1></div>)
+}
 function App() {
   return (
+
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={Hats} />
+      </Switch>
+
     </div>
   );
 }
---
+++ src/components/menu-items/menu-items.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { withRouter } from "react-router-dom"
+import "./menu-items.styles.scss"
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div style={{
+    backgroundImage: `url(${imageUrl})`
+  }} className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitile">Shop Now</span>
+    </div>
+  </div>
+)
+
+export default withRouter(MenuItem) 
