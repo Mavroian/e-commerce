a984976fb859ff8c5644cc659c44e902cf754f48
mavroian <mavflorin@gmail.com>
2020-01-12 14:32:45 +0900

    added subscrition state and auth status

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css'
 import { Route, Switch } from "react-router-dom"
 import HomePage from "./pages/homepage/Homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import { Header } from "./components/header/header.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
+import { auth } from "./firebase/firebase.utils"
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null;
 
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+    })
+  }
 
-    </div>
-  );
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+  render() {
+    return (
+
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+
+      </div>
+    );
+  }
 }
 
 export default App;
