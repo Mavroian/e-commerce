6eb3d82db79268c771a1cb635e8d345aae4a937a
mavroian <mavflorin@gmail.com>
2020-01-13 11:03:02 +0900

    added signup component to sign in and sign up page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import "./sign-in-and-sign-up.styles.scss"
 import SignIn from "../../components/sign-in/sign-in.component"
+import SignUp from "../../components/sign-up/sign-up.component"
 
 class SignInAndSignUpPage extends Component {
   constructor() {
@@ -10,6 +11,7 @@ class SignInAndSignUpPage extends Component {
     return (
       <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
       </div>
     )
   }
