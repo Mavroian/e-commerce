b7dce0787bacf3b7f63334458212e64413174424
mavroian <mavflorin@gmail.com>
2020-01-13 14:40:43 +0900

    created notifications

---
+++ src/notification-messages/notifications.-messages.js
@@ -0,0 +1,21 @@
+import { NotificationManager } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+
+const createNotification = (type, message, error) => {
+  switch (type) {
+    case 'info':
+      NotificationManager.info(`${message}`);
+      break;
+    case 'success':
+      NotificationManager.success('Success', `${message}`, 1000);
+      break;
+    case 'warning':
+      NotificationManager.warning('Warning', `${message}`, 3000);
+      break;
+    case 'error':
+      NotificationManager.error('Try Again', `${error.message}`, 5000);//it can take a callback
+      break;
+  };
+};
+
+export default createNotification
