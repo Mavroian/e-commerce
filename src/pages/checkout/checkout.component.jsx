8d6b81851c16566d7a61276a7cf6726e50e6604f
mavroian <mavflorin@gmail.com>
2020-01-15 22:03:15 +0900

    added checkout page

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import "./checkout.styles.scss"
+import { connect } from "react-redux"
+import { selectCartItems } from "../../redux/cart/cart.selectors"
+import { selectCartItemsTotal } from "../../redux/cart/cart.selectors"
+import CheckoutItem from "../../components/checkout-item/checkout-item.component"
+
+const CheckoutPage = ({ cartItems, total }) => {
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+
+      </div>
+      <div>
+        {
+          cartItems.map(item => (
+            <CheckoutItem key={item.id} cartItem={item} />
+          ))
+        }
+      </div>
+      <div className="total">
+        <span>Total:${total}</span>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cartItems: selectCartItems(state),
+    total: selectCartItemsTotal(state)
+  }
+}
+export default connect(mapStateToProps)(CheckoutPage)
