b2e84e15805e577fa22cdd0032def875ebc31a3f
mavroian <mavflorin@gmail.com>
2020-01-15 22:02:57 +0900

    using selectors in header comp

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,8 @@ import { auth } from "../../firebase/firebase.utils"
 import { connect } from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component"
 import CartDropdown from "../cart-dropdown/cart-dropdown.component"
+import { selectCurrentUser } from "../../redux/user/user.selectors"
+import { selectCartHidden } from "../../redux/cart/cart.selectors"
 
 const Header = ({ currentUser, hidden }) => {
   return (
@@ -40,8 +42,8 @@ const Header = ({ currentUser, hidden }) => {
 }
 const mapStateToProps = (state) => {
   return {
-    currentUser: state.user.currentUser,
-    hidden: state.cart.hidden
+    currentUser: selectCurrentUser(state),
+    hidden: selectCartHidden(state)
   }
 }
 
