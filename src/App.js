7cfa9b5296628ea184d1048fa154a91500719204
mavroian <mavflorin@gmail.com>
2020-01-15 22:01:25 +0900

    Added checkout page, selector

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Route, Switch, Redirect } from "react-router-dom"
 import HomePage from "./pages/homepage/Homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
+import CheckoutPage from "./pages/checkout/checkout.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
+import { selectCurrentUser } from "./redux/user/user.selectors"
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 import { connect } from "react-redux"
 import { setCurrentUser } from "./redux/user/user.actions"
@@ -44,17 +46,18 @@ class App extends Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : <SignInAndSignUpPage />} />
+          <Route path="/checkout" component={CheckoutPage} />
         </Switch>
 
       </div>
     );
   }
 }
-const mapStateToPRops = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = (state) => ({
+  currentUser: selectCurrentUser(state)
 })
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(mapStateToPRops, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
