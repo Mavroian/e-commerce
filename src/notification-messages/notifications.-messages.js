4242ca49e442699c36307a50fe448b5e230a9abd
mavroian <mavflorin@gmail.com>
2020-02-04 21:59:40 +0900

    added default case

--- src/notification-messages/notifications.-messages.js
+++ src/notification-messages/notifications.-messages.js
@@ -1,21 +1,22 @@
-import { NotificationManager } from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
+import { NotificationManager } from "react-notifications";
+import "react-notifications/lib/notifications.css";
 
 const createNotification = (type, message, error) => {
   switch (type) {
-    case 'info':
+    case "info":
       NotificationManager.info(`${message}`);
       break;
-    case 'success':
-      NotificationManager.success('Success', `${message}`, 1000);
+    case "success":
+      NotificationManager.success("Success", `${message}`, 1000);
       break;
-    case 'warning':
-      NotificationManager.warning('Warning', `${message}`, 3000);
+    case "warning":
+      NotificationManager.warning("Warning", `${message}`, 3000);
       break;
-    case 'error':
-      NotificationManager.error('Try Again', `${error.message}`, 5000);//it can take a callback
+    case "error":
+      NotificationManager.error("Try Again", `${error.message}`, 5000); //it can take a callback
       break;
-  };
+    default:
+      break;
+  }
 };
-
-export default createNotification
+export default createNotification;
