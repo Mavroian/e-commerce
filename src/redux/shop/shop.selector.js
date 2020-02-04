727760e9830389f000baa71e1f75d9b4627c8a98
mavroian <mavflorin@gmail.com>
2020-02-04 22:07:17 +0900

    created a selector for data object and nomalized data

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,18 @@
-import { createSelector } from "reselect"
+import { createSelector } from "reselect";
 
 //input selector
-const selectShop = (state) => state.shop
-
-
+const selectShop = state => state.shop;
 //output selector
-export const selectShopCollections = createSelector([selectShop], (shop) => shop.collections)
+export const selectShopCollections = createSelector([selectShop], shop => shop);
+
+export const selectCollectionForPreview = createSelector(
+  [selectShop],
+  collections => {
+    return Object.keys(collections).map(key => collections[key]);
+  }
+);
+export const selectCollection = collectionUrlParam => {
+  return createSelector([selectShopCollections], collections => {
+    return collections[collectionUrlParam];
+  });
+};
