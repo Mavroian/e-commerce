8706e1cacc035c79ab7b8a87b1a27178220a4422
mavroian <mavflorin@gmail.com>
2020-02-04 22:06:29 +0900

    removed constructor function

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from "react"
-import "./sign-in-and-sign-up.styles.scss"
-import SignIn from "../../components/sign-in/sign-in.component"
-import SignUp from "../../components/sign-up/sign-up.component"
+import React, { Component } from "react";
+import "./sign-in-and-sign-up.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 class SignInAndSignUpPage extends Component {
-  constructor() {
-    super()
-  }
   render() {
     return (
       <div className="sign-in-and-sign-up">
         <SignIn />
         <SignUp />
       </div>
-    )
+    );
   }
 }
 
-export default SignInAndSignUpPage
+export default SignInAndSignUpPage;
